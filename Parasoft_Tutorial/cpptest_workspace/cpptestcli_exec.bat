706ab5d6205662d8fdf701416a4a8f163a7429f2
yosuzuki <yosuzuki@techmatorix.co.jp>
2026-03-06 09:45:17 +0900

    a

--- Parasoft_Tutorial/cpptest_workspace/cpptestcli_exec.bat
+++ Parasoft_Tutorial/cpptest_workspace/cpptestcli_exec.bat
@@ -1,6 +1,6 @@
-set BUILD_TOOL_CHAIN_DIR=C:\cygwin64
-set CPPTEST_DIR=C:\Parasoft\20252\cpptest
-set PATH=%BUILD_TOOL_CHAIN_DIR%\bin;%CPPTEST_DIR%;%PATH%
+REM set BUILD_TOOL_CHAIN_DIR=C:\cygwin64
+REM set CPPTEST_DIR=C:\Parasoft\20252\cpptest
+REM set PATH=%BUILD_TOOL_CHAIN_DIR%\bin;%CPPTEST_DIR%;%PATH%
 
 set WORKSPACE_DIR=%~dp0
 set PROJECT_NAME=FlowAnalysis
@@ -8,6 +8,7 @@ set PROJECT_NAME=FlowAnalysis
 cpptestcli ^
 -data %WORKSPACE_DIR% ^
 -bdf %WORKSPACE_DIR%\%PROJECT_NAME%\cpptestscan.bdf ^
+-settings %WORKSPACE_DIR%\settings.properties ^
 -resource %PROJECT_NAME% ^
 -config "%~dp0..\tools\TestConfig\1_フロー解析（チュートリアル）.properties" ^
 -report %WORKSPACE_DIR%\report ^
